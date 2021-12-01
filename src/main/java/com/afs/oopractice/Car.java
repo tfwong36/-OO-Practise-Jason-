f2427bc91428a58fe72c85d825b8d6b147415819
tfwong36 <tfwong36@gmail.com>
2021-12-01 16:36:55 +0800

    [oo-practice][Jason]feat: implement AC3

--- src/main/java/com/afs/oopractice/Car.java
+++ src/main/java/com/afs/oopractice/Car.java
@@ -1,15 +1,7 @@
 package com.afs.oopractice;
 
-public class Car {
-    private String name;
-    private int speed;
-
+public class Car extends Vehicle {
     public Car(String name, int speed) {
-        this.name = name;
-        this.speed = speed;
-    }
-
-    public void printSpeedUp(){
-        System.out.printf("%s : speed up %d km/h\n", name, speed);
+        super (name, speed);
     }
 }
--- src/main/java/com/afs/oopractice/HelloWorld.java
+++ src/main/java/com/afs/oopractice/HelloWorld.java
@@ -5,7 +5,13 @@ public class HelloWorld {
     public static void main(String[] args){
         Car car = new Car("Car", 30);
         car.printSpeedUp();
+
         Truck truck = new Truck("Truck", 10);
         truck.printSpeedUp();
+
+        Driver carDriver = new Driver(new Car("Car", 30));
+        Driver truckDriver = new Driver(new Truck("Truck", 10));
+        carDriver.printSpeedUp();
+        truckDriver.printSpeedUp();
     }
 }
