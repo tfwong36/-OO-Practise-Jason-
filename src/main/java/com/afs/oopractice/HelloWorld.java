b1bc992db26cc4b34d959010c2f6320979d911f5
tfwong36 <tfwong36@gmail.com>
2021-12-01 16:14:00 +0800

    first commit

---
+++ src/main/java/com/afs/oopractice/HelloWorld.java
@@ -0,0 +1,4 @@
+package com.afs.oopractice;
+
+public class HelloWorld {
+}
---
+++ src/test/java/com/afs/oopratice/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package com.afs.oopratice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    void should__when__given_() {
+        //given
+
+        //when
+
+        //then
+        assertEquals(1, 1);
+    }
+
+}
